c82b13dc9fcc8de04e9b902c2e419cb0c972207f
Gavin Sykes <gavin@gavinsykes.uk>
2020-05-19 17:34:08 +0100

    Padding out the code

--- src/index.ts
+++ src/index.ts
@@ -26,46 +26,91 @@ class SVGChrtColumn extends SVGChrt {
                ? this.data // callbacks like sort and map
                : undefined,
       left   : this.settings.chart.axes.left.display
-               ? [0, d3.max(data, d => d.value)]
+               ? switch(type) {
+                   case 'clustered':
+                     [0, d3.max(data, d => d.value)];
+                     break;
+                   case 'stacked':
+                     [0, d3.max(sum)];
+                     break;
+                   case 'stacked100':
+                     [0, 100];
+                     break;
+                 }
                : undefined,
       top    : this.settings.chart.axes.top.display
                ? this.data
                : undefined,
       right  : this.settings.chart.axes.right.display
-               ? [0, d3.max(data, d => d.value)]
+               ? switch(type) {
+                   case 'clustered':
+                     [0, d3.max(data, d => d.value)];
+                     break;
+                   case 'stacked':
+                     [0, d3.max(sum)];
+                     break;
+                   case 'stacked100':
+                     [0, 100];
+                     break;
+                 }
                : undefined
     };
     const ranges = {
       bottom : domains.bottom
-               ? [0, this.getChartArea().width]
+               ? [
+                   0,
+                   this.getChartArea().width
+                 ]
                : undefined,
       left   : domains.left
-               ? [this.getChartArea().height, 0]
+               ? [
+                   this.getChartArea().height,
+                   0
+                 ]
                : undefined,
       top    : domains.top
-               ? [0, this.getChartArea().width]
+               ? [
+                   0,
+                   this.getChartArea().width
+                 ]
                : undefined,
       right  : domains.right
-               ? [this.getChartArea().height, 0]
+               ? [
+                   this.getChartArea().height,
+                   0
+                 ]
                : undefined
     };
     const scales = {
       bottom : ranges.bottom
-               ? d3.scaleBand().domain(domains.bottom).range(ranges.bottom).padding(0.1) // Variable for padding?
+               ? d3.scaleBand()
+                   .domain(domains.bottom)
+                   .range(ranges.bottom)
+                   .padding(0.1) // Variable for padding?
                : undefined,
       left   : ranges.left
-               ? d3.scaleLinear().domain(domains.left).range(ranges.left).nice() // Options for things like scaleLog etc.
+               ? d3.scaleLinear()
+                   .domain(domains.left)
+                   .range(ranges.left)
+                   .nice() // Options for things like scaleLog etc.
                : undefined,
       top    : ranges.top
-               ? d3.scaleBand().domain(domains.top).range(ranges.top).padding(0.1)
+               ? d3.scaleBand()
+                   .domain(domains.top)
+                   .range(ranges.top)
+                   .padding(0.1)
                : undefined,
       right  : ranges.right
-               ? d3.scaleLinear().domain(domains.right).range(ranges.right).nice() // Same options as on left
+               ? d3.scaleLinear()
+                   .domain(domains.right)
+                   .range(ranges.right)
+                   .nice() // Same options as on left
                : undefined
     };
     const axisFuncs = {
       bottom : scales.bottom
-               ? d3.axisBottom(scales.bottom).ticks(domains.bottom.length)
+               ? d3.axisBottom(scales.bottom)
+                   .ticks(domains.bottom.length)
                : undefined,
       left   : scales.right
                ? d3.axisLeft(scales.left) // Callbacks like tickFormat
@@ -80,59 +125,107 @@ class SVGChrtColumn extends SVGChrt {
 
     const axes = {
       bottom : axisFuncs.bottom
-               ? this.appendSVGChild('g', this.chartArea, {class: 'axis x-axis bottom-axis'})
+               ? this.appendSVGChild('g', this.chartArea, {
+                   class : 'axis x-axis bottom-axis'
+                 })
                : undefined,
       left   : axisFuncs.left
-               ? this.appendSVGChild('g', this.chartArea, {class: 'axis y-axis left-axis'})
+               ? this.appendSVGChild('g', this.chartArea, {
+                   class : 'axis y-axis left-axis'
+                 })
                : undefined,
       top    : axisFuncs.top
-               ? this.appendSVGChild('g', this.chartArea, {class: 'axis x-axis top-axis'})
+               ? this.appendSVGChild('g', this.chartArea, {
+                   class : 'axis x-axis top-axis'
+                 })
                : undefined,
       right  : axisFuncs.right
-               ? this.appendSVGChild('g', this.chartArea, {class: 'axis y-axis right-axis'})
+               ? this.appendSVGChild('g', this.chartArea, {
+                   class : 'axis y-axis right-axis'
+                 })
                : undefined
     };
 
     const axisScales = {
       bottom : axes.bottom
-               ? this.appendSVGChild('g', axes.bottom, {class: 'axis-scale x-axis-scale bottom-axis-scale'})
+               ? this.appendSVGChild('g', axes.bottom, {
+                   class : 'axis-scale x-axis-scale bottom-axis-scale'
+                 })
                : undefined,
       left   : axes.left
-               ? this.appendSVGChild('g', axes.left, {class: 'axis-scale y-axis-scale left-axis-scale'})
+               ? this.appendSVGChild('g', axes.left, {
+                   class : 'axis-scale y-axis-scale left-axis-scale'
+                 })
                : undefined,
       top    : axes.top
-               ? this.appendSVGChild('g', axes.top, {class: 'axis-scale x-axis-scale top-axis-scale'})
+               ? this.appendSVGChild('g', axes.top, {
+                   class : 'axis-scale x-axis-scale top-axis-scale'
+                 })
                : undefined,
       right  : axes.right
-               ? this.appendSVGChild('g', axes.right, {class: 'axis-scale y-axis-scale right-axis-scale'})
+               ? this.appendSVGChild('g', axes.right, {
+                   class : 'axis-scale y-axis-scale right-axis-scale'
+                 })
                : undefined
     };
 
     if (axisScales.bottom) {
-      d3.select(axisScales.bottom).call(axisFuncs.bottom);
+      d3.select(axisScales.bottom)
+        .call(axisFuncs.bottom);
     }
     if (axisScales.left) {
-      d3.select(axisScales.left).call(axisFuncs.left);
+      d3.select(axisScales.left)
+        .call(axisFuncs.left);
     }
     if (axisScales.right) {
-      d3.select(axisScales.right).call(axisFuncs.right);
+      d3.select(axisScales.right)
+        .call(axisFuncs.right);
     }
     if (axisScales.top) {
-      d3.select(axisScales.top).call(axisFuncs.top);
+      d3.select(axisScales.top)
+        .call(axisFuncs.top);
     }
 
     const axisLabels = {
-      bottom : axes.bottom && settings.chart.axes.bottom.label.display && settings.chart.axes.bottom.label.text
-               ? this.appendSVGChild('text', axes.bottom, {class: 'axis-label x-axis-label bottom-axis-label', dy: '1em', 'text-anchor': 'middle', transform: `translate(0,${axisScales.bottom.getBBox().height + 4})`}, 'Bottom Label')
+      bottom : axes.bottom &&
+               settings.chart.axes.bottom.label.display &&
+               settings.chart.axes.bottom.label.text
+               ? this.appendSVGChild('text', axes.bottom, {
+                   class         : 'axis-label x-axis-label bottom-axis-label',
+                   dy            : '1em',
+                   'text-anchor' : 'middle',
+                   transform     : `translate(0,${axisScales.bottom.getBBox().height + 4})`
+                 }, 'Bottom Label')
                : undefined,
-      left   : axes.left && settings.chart.axes.left.label.display && settings.chart.axes.left.label.text
-               ? this.appendSVGChild('text', axes.left, {class: 'axis-label y-axis-label left-axis-label', dy: '1em', 'text-anchor': 'middle', transform: `rotate(270)`}, 'Left Label')
+      left   : axes.left &&
+               settings.chart.axes.left.label.display &&
+               settings.chart.axes.left.label.text
+               ? this.appendSVGChild('text', axes.left, {
+                   class         : 'axis-label y-axis-label left-axis-label',
+                   dy            : '1em',
+                   'text-anchor' : 'middle',
+                   transform     : `rotate(270)`
+                 }, 'Left Label')
                : undefined,
-      top    : axes.top && settings.chart.axes.top.label.display && settings.chart.axes.top.label.text
-               ? this.appendSVGChild('text', axes.top, {class: 'axis-label x-axis-label top-axis-label', dy: '1em', 'text-anchor': 'middle', transform: `translate(0,0)`}, 'Top Label')
+      top    : axes.top &&
+               settings.chart.axes.top.label.display &&
+               settings.chart.axes.top.label.text
+               ? this.appendSVGChild('text', axes.top, {
+                   class         : 'axis-label x-axis-label top-axis-label',
+                   dy            : '1em',
+                   'text-anchor' : 'middle',
+                   transform     : `translate(0,0)`
+                 }, 'Top Label')
                : undefined,
-      right  : axes.right && settings.chart.axes.right.label.display && settings.chart.axes.right.label.text
-               ? this.appendSVGChild('text', axes.right, {class: 'axis-label y-axis-label right-axis-label', dy: '1em', 'text-anchor': 'middle', transform: `rotate(270)`}, 'Right Label')
+      right  : axes.right &&
+               settings.chart.axes.right.label.display &&
+               settings.chart.axes.right.label.text
+               ? this.appendSVGChild('text', axes.right, {
+                   class         : 'axis-label y-axis-label right-axis-label',
+                   dy            : '1em',
+                   'text-anchor' : 'middle',
+                   transform     : `rotate(270)`
+                 }, 'Right Label')
                : undefined
     };
 
